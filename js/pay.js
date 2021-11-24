1d0bcb031dd3ca8b004a6bd2a6b01a69543c2ab9
Matt <matthew.schultz1992@gmail.com>
2021-11-24 10:21:34 +1000

    first commit

---
+++ js/pay.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  $('#save').on('click', function() {
+    const pay = $('#pay').val();
+    const perSecond = pay/52/5/8/60/60;
+    let total = 0;
+    window.setInterval(function() {
+      total += perSecond
+      $('#runningTotal').text(total);
+    }, 1000);
+  });
+
+
+
+});
